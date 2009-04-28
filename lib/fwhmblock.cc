4c84427e03a19fcb85166c873b5aaf531823156c
Domas Monkus <domas.m@gmail.com>
2009-04-28 22:41:50 +0300

    FWHM block taking margins into account.

--- lib/fwhmblock.cc
+++ lib/fwhmblock.cc
@@ -16,14 +16,17 @@ FWHMBlock::FWHMBlock(DataBlock * data)
 void FWHMBlock::CalculateIntensity() {
   long * intensity = new long[size_x*size_y];
   long max = 0;
-  for (int i = 0; i < size_x*size_y; i++) {
-    intensity[i] = 0;
-    //each point
-    for (int k = 0; k < original->GetZ(); k++) {
-      intensity[i] += original->block[k][i];
-    }
-    if (intensity[i] > max) {
-      max = intensity[i];
+  for (int x = x_low; x < x_high; x++) {
+    for (int y = y_low; y < y_high; y++) {
+      int i = x + y*GetX();
+      intensity[i] = 0;
+      //each point
+      for (int z = 0; z < original->GetZ(); z++) {
+        intensity[i] += original->GetPoint(x, y, z);
+      }
+      if (intensity[i] > max) {
+        max = intensity[i];
+      }
     }
   }
   for (int i = 0; i < size_x*size_y; i++) {
@@ -37,50 +40,54 @@ void FWHMBlock::CalculateFWHM() {
   int max = 0;
   int max_index = 0;
 
-  for (int i = 0; i < size_x*size_y; i++) {
-    block[1][i] = 0;
-    for (int j = 0; j < original->GetZ(); j++) {
-      vector[j] = original->block[j][i];
-      if (vector[j] > max) {
-        max = vector[j];
-        max_index = j;
+  for (int x = x_low; x < x_high; x++) {
+    for (int y = y_low; y < y_high; y++) {
+      int i = x + y*GetX();      
+      block[1][i] = 0;
+      for (int z = 0; z < original->GetZ(); z++) {
+        vector[z] = original->GetPoint(x, y, z);
+        if (vector[z] > max) {
+          max = vector[z];
+          max_index = z;
+        }
       }
-    }
-
-    int j = 0;
-    float left_ind;
-    float right_ind;
-    bool found_left = false;
-    bool found_right = false;
     
-    float half = (float)max / 2;
 
-    while( ((max_index - j >= 0) || (max_index + j < original->GetZ())) &&
-           !(found_left && found_right)) {
-      //check left side
-      int index = max_index - j;
-      if ((index >= 0) && !found_left) {
-        if (vector[index] < half) {
-          left_ind = index - (half - vector[index]) / (vector[index + 1] - vector[index]);
-          found_left = true;
-        } else {
-          left_ind = index;
+      int j = 0;
+      float left_ind;
+      float right_ind;
+      bool found_left = false;
+      bool found_right = false;
+    
+      float half = (float)max / 2;
+
+      while( ((max_index - j >= 0) || (max_index + j < original->GetZ())) &&
+             !(found_left && found_right)) {
+        //check left side
+        int index = max_index - j;
+        if ((index >= 0) && !found_left) {
+          if (vector[index] < half) {
+            left_ind = index - (half - vector[index]) / (vector[index + 1] - vector[index]);
+            found_left = true;
+          } else {
+            left_ind = index;
+          }
         }
-      }
       
-      //check the right side
-      index = max_index + j;
-      if ((index < original->GetZ()) && !found_right) {
-        if (vector[index] < half) {
-          right_ind = index - (half - vector[index]) / (vector[index - 1] - vector[index]);
-          found_right = true;
-        } else {
-          right_ind = index;
+        //check the right side
+        index = max_index + j;
+        if ((index < original->GetZ()) && !found_right) {
+          if (vector[index] < half) {
+            right_ind = index - (half - vector[index]) / (vector[index - 1] - vector[index]);
+            found_right = true;
+          } else {
+            right_ind = index;
+          }
         }
+        j++;
       }
-      j++;
+      block[1][i] = std::abs(lround(right_ind - left_ind));
     }
-    block[1][i] = abs(round(right_ind - left_ind));
   }
 }
           
