1a40b2c2b3617ed95a1781b66e8f0cb982cc5248
Domas Monkus <domas.m@gmail.com>
2009-04-29 10:06:52 +0300

    Sizing the dialog.

--- ui/marginsdialog.cc
+++ ui/marginsdialog.cc
@@ -21,70 +21,76 @@ MarginsDialog::MarginsDialog(wxWindow * parent, wxWindowID id, DataBlock * block
   block->GetMarginX(lo, hi);
   wxBoxSizer * row = new wxBoxSizer(wxHORIZONTAL);
   wxStaticText * text = new wxStaticText(this, wxID_ANY, _("X min:"));
-  row->Add(text);
+  row->Add(text, 1);
   x_low = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                          wxSP_ARROW_KEYS, 0, block->GetX(), lo);
   row->Add(x_low);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   row = new wxBoxSizer(wxHORIZONTAL);
   text = new wxStaticText(this, wxID_ANY, _("X max:"));
-  row->Add(text);
+  row->Add(text, 1);
   x_high = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                           wxSP_ARROW_KEYS, 0, block->GetX(), hi);
   row->Add(x_high);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   block->GetMarginY(lo, hi);
   row = new wxBoxSizer(wxHORIZONTAL);
   text = new wxStaticText(this, wxID_ANY, _("Y min:"));
-  row->Add(text);
+  row->Add(text, 1);
   y_low = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                          wxSP_ARROW_KEYS, 0, block->GetY(), lo);
   row->Add(y_low);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   row = new wxBoxSizer(wxHORIZONTAL);
   text = new wxStaticText(this, wxID_ANY, _("Y max:"));
-  row->Add(text);
+  row->Add(text, 1);
   y_high = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                           wxSP_ARROW_KEYS, 0, block->GetY(), hi);
   row->Add(y_high);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   block->GetMarginZ(lo, hi);
   row = new wxBoxSizer(wxHORIZONTAL);
   text = new wxStaticText(this, wxID_ANY, _("T min:"));
-  row->Add(text);
+  row->Add(text, 1);
   z_low = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                          wxSP_ARROW_KEYS, 0, block->GetZ(), lo);
   row->Add(z_low);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   row = new wxBoxSizer(wxHORIZONTAL);
   text = new wxStaticText(this, wxID_ANY, _("T max:"));
-  row->Add(text);
+  row->Add(text, 1);
   z_high = new wxSpinCtrl(this, wxID_ANY, wxEmptyString,
                          wxDefaultPosition, wxDefaultSize,
                           wxSP_ARROW_KEYS, 0, block->GetZ(), hi);
   row->Add(z_high);
-  sizer->Add(row);
+  sizer->Add(row, 0, wxEXPAND);
 
   wxButton * ok = new wxButton(this, ID_MD_Yes, _("Ok"));
-  sizer->Add(ok);
+  sizer->Add(ok, 0, wxEXPAND);
 
   wxButton * no = new wxButton(this, ID_MD_No, _("Cancel"));
-  sizer->Add(no);
+  sizer->Add(no, 0, wxEXPAND);
 
   wxButton * reset = new wxButton(this, ID_MD_Reset, _("Reset"));
-  sizer->Add(reset);
+  sizer->Add(reset, 0, wxEXPAND);
   
   SetSizer(sizer);
+
+  sizer->SetSizeHints(this);
+  sizer->SetSizeHints(this);
+
+  sizer->Layout();
+  sizer->Fit(this);
 }
 
 MarginsDialog::~MarginsDialog() {
